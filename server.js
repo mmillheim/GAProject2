a750816098928292e4e71c0d16c6df3bc4f3aa4e
mmillheim <m.millheim@gmail.com>
2021-06-28 22:16:44 -0400

    added router for Auth0

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const ejsLayouts = require('express-ejs-layouts');
-const path = require('path')
+const path = require('path');
 const sassMiddleware = require('node-sass-middleware');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+
+//Auth0
+const { auth } = require('express-openid-connect');
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'https://levelsapp.herokuapp.com',
+    clientID: '7JUChbVDV0yWzajl4gOh2SOMIajPBlCE',
+    issuerBaseURL: 'https://levelsapp.us.auth0.com'
+  };
+
+app.use(auth(config));
 
 
 app.set('view engine', 'ejs');
@@ -32,12 +46,12 @@ app.get('/welcome', (req, res) => {
     res.send('welcome')
 })
 
-app.get('/login', (req, res) => {
-    res.send("Please Log In")
-})
+// app.get('/login', (req, res) => {
+//     res.send("Please Log In")
+// })
 
 app.get('/', (req, res) => {
-    res.redirect('/welcome');
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 })
 
 app.listen(app.get("port"), () => {
